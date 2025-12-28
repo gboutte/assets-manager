81ca4d8c54b6db40f9338894d8682319f005644a
gboutte <btte.gregory@gmail.com>
2025-12-28 12:48:23 +0100

    feat: handle subdirectory upload

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,11 @@
 
 use std::collections::HashMap;
 use std::io;
-use std::path::Path;
+use std::path::{Path, PathBuf};
+use filesize::file_real_size;
 use rocket::Data;
 use rocket::data::ToByteUnit;
+use rocket::fs::TempFile;
 use rocket::http::Method;
 use rocket::http::uri::Absolute;
 use rocket::response::content::RawText;
@@ -29,12 +31,22 @@ async fn get_asset(file_name:String) -> Option<RawText<File>> {
     File::open(full_path).await.map(RawText).ok()
 }
 
-#[post("/<file_name>", data = "<file>")]
-async fn post_asset(file_name:String,file: Data<'_>)  -> io::Result<String> {
+#[post("/<file_name_buf..>", data = "<file>")]
+async fn post_asset(file_name_buf:PathBuf, mut file:TempFile<'_>)  -> io::Result<String> {
+
+    let file_name = file_name_buf.clone().into_os_string().into_string().unwrap();
+    let mimetype = file.content_type().unwrap();
+    let filesize = file_real_size(file.path().unwrap()).unwrap();
+    print!("File {}, type: {}, size: {}",file_name,mimetype,filesize);
 
     let root = concat!(env!("CARGO_MANIFEST_DIR"), "/", "upload");
     let full_path = Path::new(root).join(&file_name);
-    file.open(128.kibibytes()).into_file(full_path).await?;
+    
+    let prefix = full_path.parent().unwrap();
+    std::fs::create_dir_all(prefix).unwrap();
+    file.persist_to(full_path).await?;
+    
+    
     Ok(uri!(HOST ,get_asset(file_name)).to_string())
 }
 
